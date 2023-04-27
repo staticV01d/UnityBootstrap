52962e3ee2322a2788137a43f1a5b7cead091154
staticVoid <klpummell.dev@gmail.com>
2023-04-26 20:00:56 -0700

    Update InteractiveSwitch.cs

--- FirstPersonBootstrap/Assets/Scripts/InteractiveSwitch.cs
+++ FirstPersonBootstrap/Assets/Scripts/InteractiveSwitch.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractiveSwitch : MonoBehaviour
+public class InteractiveSwitch : Interact
 {
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+
+    protected override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
     }
 }
