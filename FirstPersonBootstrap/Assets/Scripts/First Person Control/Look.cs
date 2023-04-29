bd0cbefc7e629e82dca2ee1db7f25857a422a106
staticVoid <klpummell.dev@gmail.com>
2023-04-28 18:57:11 -0700

    Update Look.cs

--- FirstPersonBootstrap/Assets/Scripts/First Person Control/Look.cs
+++ FirstPersonBootstrap/Assets/Scripts/First Person Control/Look.cs
@@ -20,12 +20,15 @@ public class Look : MonoBehaviour
 
     void Update()
     {
-        Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
-        appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
-        currentMouseLook += appliedMouseDelta;
-        currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
+        if (Time.timeScale > 0)
+        {
+            Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
+            appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
+            currentMouseLook += appliedMouseDelta;
+            currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
 
-        charCamera.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
-        transform.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
+            charCamera.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
+            transform.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
+        }
     }
 }
